bf41f0939ece18ec3503f09ba887d13c14138064
rjunio98 <robledorjunior1@gmail.com>
2024-05-20 03:09:19 -0300

    feat: Add user authentication endpoints

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,11 @@
 import { Router, Request, Response } from "express";
+import { CreateUserController } from "./controllers/user/CreateUserController";
+import { AuthController } from "./controllers/user/AuthUserController";
 
 const router = Router();
 
-router.get("/teste", (req: Request, res: Response) => {
-  return res.json({ nome: "teste" });
-});
+router.post("/users", new CreateUserController().handle);
+
+router.post("/session", new AuthController().handle);
 
 export { router };
