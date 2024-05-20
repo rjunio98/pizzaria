4af1b5ed02f2f31a5e3c8432402f599b9ec0214c
rjunio98 <robledorjunior1@gmail.com>
2024-05-20 05:47:26 -0300

    feat: Add detail user endpoint and authentication middleware

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,11 +1,16 @@
 import { Router, Request, Response } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthController } from "./controllers/user/AuthUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
+
+import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
 
 router.post("/users", new CreateUserController().handle);
 
 router.post("/session", new AuthController().handle);
+
+router.get("/me", isAuthenticated, new DetailUserController().handle);
 
 export { router };
