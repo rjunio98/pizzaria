fd5918e13769729683d307fb122836126cf6f94f
rjunio98 <robledorjunior1@gmail.com>
2024-05-20 06:53:59 -0300

    feat: Add CreateCategoryController and CreateCategoryService

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,7 @@ import { AuthController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 
 const router = Router();
 
@@ -12,5 +13,11 @@ router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthController().handle);
 
 router.get("/me", isAuthenticated, new DetailUserController().handle);
+
+router.post(
+  "/category",
+  isAuthenticated,
+  new CreateCategoryController().handle
+);
 
 export { router };
