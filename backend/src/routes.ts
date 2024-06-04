f0f0602844421916aee58d388c0e1a49b6b95501
rjunio98 <robledorjunior1@gmail.com>
2024-06-04 12:11:23 -0300

    feat: filtro produtos por categoria

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -9,6 +9,8 @@ import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
 import { CreateProductController } from "./controllers/product/CreateProductController";
+import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
+
 import uploadConfig from "./config/multer";
 
 const router = Router();
@@ -34,6 +36,12 @@ router.post(
   isAuthenticated,
   upload.single("file"),
   new CreateProductController().handle
+);
+
+router.get(
+  "/category/product",
+  isAuthenticated,
+  new ListByCategoryController().handle
 );
 
 export { router };
