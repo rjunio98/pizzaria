692fd18bc280519fa03d09a1c9a2edc4a97fe741
rjunio98 <robledorjunior1@gmail.com>
2024-06-12 02:34:35 -0300

    feat: enviando pedido

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -15,6 +15,7 @@ import { CreateOrderController } from "./controllers/order/CreateOrderController
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
 import { AddItemController } from "./controllers/order/AddItemController";
 import { RemoveItemController } from "./controllers/order/RemoveItemController";
+import { SendOrderController } from "./controllers/order/SendOrderController";
 
 import uploadConfig from "./config/multer";
 
@@ -57,5 +58,6 @@ router.delete(
   isAuthenticated,
   new RemoveItemController().handle
 );
+router.put("/order/send", isAuthenticated, new SendOrderController().handle);
 
 export { router };
